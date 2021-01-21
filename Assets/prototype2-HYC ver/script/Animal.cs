dd5f108f3a09191e4a17065ee1145c940e3c1057
overpowerhero <71799083+overpowerhero@users.noreply.github.com>
2021-01-21 19:12:22 +0800

    0121-修正FPS值問題

--- Assets/prototype2-HYC ver/script/Animal.cs
+++ Assets/prototype2-HYC ver/script/Animal.cs
@@ -7,6 +7,20 @@ using UnityEngine;
 public class Animal : MonoBehaviour
 {
 
+    private float m_LastUpdateShowTime = 0f;    //上一次更新幀率的時間;
+
+    private float m_UpdateShowDeltaTime = 0.01f;//更新幀率的時間間隔;
+
+    private int m_FrameUpdate = 0;//幀數;
+
+    private float m_FPS = 0;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+
     public float speed;//0114 張琇媗
 
     
@@ -19,8 +33,12 @@ public class Animal : MonoBehaviour
         //lok 0108-HYC
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Human");
+
+
+
+        m_LastUpdateShowTime = Time.realtimeSinceStartup;//更新FPS
     }
-     
+
     // Update is called once per frame
     void Update()
     {
@@ -29,6 +47,15 @@ public class Animal : MonoBehaviour
         enemyRb.AddForce(lookDirection * speed );
 
         transform.LookAt(player.transform);//0114 張琇媗
+
+
+        m_FrameUpdate++;
+        if (Time.realtimeSinceStartup - m_LastUpdateShowTime >= m_UpdateShowDeltaTime)
+        {
+            m_FPS = m_FrameUpdate / (Time.realtimeSinceStartup - m_LastUpdateShowTime);
+            m_FrameUpdate = 0;
+            m_LastUpdateShowTime = Time.realtimeSinceStartup;
+        }//更新FPS值
     }
 
     //0114 張琇媗
@@ -39,4 +66,10 @@ public class Animal : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2, 0, 100, 100), "FPS: " + m_FPS);
+    }//顯示FPS值
 }
